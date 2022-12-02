f858509b230e8733fd59c4a40b890d082fd6a4a9
Alexey Kolychev <kolaa99@mail.ru>
2022-12-02 19:42:37 +0300

    Add new pacage

--- main.go
+++ main.go
@@ -1,95 +1,12 @@
 package main
 
 import (
-	"context"
-	"io"
-	"os"
-
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/client"
-	"github.com/docker/docker/pkg/stdcopy"
+	"go-2i/api/docker"
 )
 
 func main() {
-	dockerID := DockerCreate()
-	DockerStart(dockerID)
-	DockerLogs(dockerID)
-	DockerRemove(dockerID)
-
-}
-
-func DockerCreate() string {
-	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		panic(err)
-	}
-
-	reader, err := cli.ImagePull(ctx, "docker.io/library/alpine", types.ImagePullOptions{})
-	if err != nil {
-		panic(err)
-	}
-
-	defer reader.Close()
-	io.Copy(os.Stdout, reader)
-
-	resp, err := cli.ContainerCreate(ctx, &container.Config{
-		Image: "alpine",
-		Cmd:   []string{"echo", "hello world"},
-		Tty:   false,
-	}, nil, nil, nil, "test")
-	if err != nil {
-		panic(err)
-	}
-
-	return resp.ID
-}
-
-func DockerStart(containerID string) {
-	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		panic(err)
-	}
-
-	if err := cli.ContainerStart(ctx, containerID, types.ContainerStartOptions{}); err != nil {
-		panic(err)
-	}
-
-	statusCh, errCh := cli.ContainerWait(ctx, containerID, container.WaitConditionNotRunning)
-	select {
-	case err := <-errCh:
-		if err != nil {
-			panic(err)
-		}
-	case <-statusCh:
-	}
-}
-
-func DockerLogs(containerID string) {
-	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		panic(err)
-	}
-
-	out, err := cli.ContainerLogs(ctx, containerID, types.ContainerLogsOptions{ShowStdout: true})
-	if err != nil {
-		panic(err)
-	}
-
-	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
-}
-
-func DockerRemove(containerID string) {
-	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		panic(err)
-	}
-
-	if err = cli.ContainerRemove(ctx, containerID, types.ContainerRemoveOptions{}); err != nil {
-		panic(err)
-	}
+	dockerID := docker.DockerCreate()
+	docker.DockerStart(dockerID)
+	docker.DockerLogs(dockerID)
+	docker.DockerRemove(dockerID)
 }
